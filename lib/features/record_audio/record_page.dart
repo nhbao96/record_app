247a7ba546c908e60e0b02964434982b2e2f52a1
HoangBao <hoangbao@HoangBaos-MacBook-Pro.local>
2022-12-31 11:56:29 +0700

    commit logic app

---
+++ lib/features/record_audio/record_page.dart
@@ -0,0 +1,147 @@
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:record_application/common/bases/base_widget.dart';
+
+class RecordPage extends StatefulWidget {
+  const RecordPage({Key? key}) : super(key: key);
+
+  @override
+  State<RecordPage> createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return PageContainer(
+        child: _RecordContainer(),
+        providers: [],
+        appBar: AppBar(
+          title: Text("Demo Audio Recording"),
+        ),);
+  }
+}
+
+class _RecordContainer extends StatefulWidget {
+  const _RecordContainer({Key? key}) : super(key: key);
+
+  @override
+  State<_RecordContainer> createState() => _RecordContainerState();
+}
+
+class _RecordContainerState extends State<_RecordContainer> {
+  final recoder = FlutterSoundRecorder();
+  bool isRecoderReady = false;
+
+  late PermissionStatus isPermisMicro;
+
+  String _currentFilePath = '', _recordedFilePath = '';
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    recoder.closeRecorder();
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initRecorder();
+
+  }
+
+  Future initRecorder() async{
+    print("\n\n\n -------> initRecorder  ,\n\n\n");
+    try{
+      isPermisMicro = await Permission.microphone.request();
+      if(!isPermisMicro.isGranted){
+        throw 'Microphone permission not grannted';
+      }
+
+    }catch(e){
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+    }
+    print("\n\n\n -------> permission ok ,\n\n\n");
+    await recoder.openRecorder();
+    print("\n\n\n -------> openRecorder ok ,\n\n\n");
+    isRecoderReady = true;
+    recoder.setSubscriptionDuration(const Duration(milliseconds: 500));
+    print("\n\n\n -------> done func record ,\n\n\n");
+
+  }
+
+  Future record() async{
+    if(!isRecoderReady){
+      return;
+    }
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+    print(tempPath);
+
+    final _fileName = 'DEMO_${DateTime.now().millisecondsSinceEpoch.toString()}';
+    _currentFilePath = '$tempPath/$_fileName';
+    await recoder!.startRecorder(toFile: _currentFilePath);
+    print("_currentFilePath = $_currentFilePath \n\n");
+  }
+
+  Future stop() async{
+    if(!isRecoderReady){
+      return;
+    }
+    final path = await recoder.stopRecorder();
+    final audioFile = File(path!);
+    print("Recorded audio : $audioFile");
+  }
+
+  String convert2digits(int num){
+    return  num.toString().padLeft(2, '0');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder<RecordingDisposition>(
+              stream: recoder.onProgress,
+              builder: (context,snapshot){
+                final duration = snapshot.hasData ? snapshot.data!.duration : Duration.zero;
+                final twoDigitMinutes = convert2digits(duration.inMinutes.remainder(60));
+                final twoDigitSecond = convert2digits(duration.inSeconds.remainder(60));
+
+                return Text('$twoDigitMinutes:${twoDigitSecond}s', style: TextStyle(color: Colors.black87,fontSize: 60,fontWeight: FontWeight.bold),);
+              }),
+          const SizedBox( height : 32),
+          ElevatedButton(
+            child: Icon(
+              recoder.isRecording ? Icons.stop : Icons.mic,
+              size: 80,
+            ),
+            onPressed: () async{
+              if(recoder.isRecording){
+                await stop();
+              }else{
+                await record();
+              }
+              setState(() {
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
